4cfadc423b8de2af430b6e615951be16122e89b3
Bruno Duarte <bruno.duarte@bis2bis.com.br>
2025-05-16 08:13:24 -0300

    ✨ Dashboard Componentizing

---
+++ src/components/DashboardMain.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="antialiased bg-gray-50 dark:bg-gray-900">
+    <!-- Navegate -->
+    <DashboardHeader />
+
+    <!-- Sidebar -->
+    <DashboardSidebar />
+
+    <!-- Main -->
+    <DashboardContent />
+  </div>
+</template>
+
+<script>
+  import DashboardHeader from '@/components/DashboardHeader.vue';
+  import DashboardSidebar from '@/components/DashboardSidebar.vue';
+  import DashboardContent from '@/components/DashboardContent.vue';
+
+  export default {
+    name: "DashboardMain",
+    components: {
+      DashboardHeader,
+      DashboardSidebar,
+      DashboardContent
+    }
+  }
+</script>
