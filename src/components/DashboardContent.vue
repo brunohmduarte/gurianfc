a3e3c23db7ebca9545e21f33929d8f7fa9c6f12a
Bruno Duarte <bruno.duarte@bis2bis.com.br>
2025-05-19 17:19:44 -0300

    ✨ Backup do dia 19/05

--- src/components/DashboardContent.vue
+++ src/components/DashboardContent.vue
@@ -1,11 +1,52 @@
 <template>
   <div class="bg-dashboard bg-no-repeat bg-cover bg-center p-5 pt-20 md:ml-64 w-65 h-screen overflow-y-auto">
     <main>
+      <div>
+        <h2 class="mb-4 text-2xl font-semibold text-gray-900 dark:text-white">Partidas</h2>
+
+      </div>
       <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mb-4">
-        <div class="border-2 border-dashed border-gray-300 rounded-lg dark:border-gray-600 h-32 md:h-64"></div>
-        <div class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-32 md:h-64"></div>
-        <div class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-32 md:h-64"></div>
-        <div class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-32 md:h-64"></div>
+        <div class="border-2 border-dashed border-gray-300 rounded-lg dark:border-gray-600 h-32 md:h-64">
+          <h4>16 de maio de 20025</h4>
+          <p>Rodada 9</p>
+          <div class="scoreboard">
+            <div class="team">
+              <div class="team_shield">
+                <img src="" alt="" />
+                <span>Team Name</span>
+              </div>
+              <div class="score">1</div>
+            </div>
+
+            <div class="versus">
+              <span>x</span>
+              <span>Em andamento</span>
+            </div>
+            
+            <div class="team">
+              <div class="team_shield">
+                <img src="" alt="" />
+                <span>Team Name</span>
+              </div>
+              <div class="score">2</div>
+            </div>
+          </div>
+        </div>
+        <div class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-32 md:h-64">
+          <h4>16 de maio de 20025</h4>
+          <p>Rodada 9</p>
+
+        </div>
+        <div class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-32 md:h-64">
+          <h4>16 de maio de 20025</h4>
+          <p>Rodada 9</p>
+
+        </div>
+        <div class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-32 md:h-64">
+          <h4>16 de maio de 20025</h4>
+          <p>Rodada 9</p>
+
+        </div>
       </div>
 
       <div class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-96 mb-4"></div>
