8378fce89ee1a2a08f4435c952a1b51529a53ea1
Bruno Duarte <bruno.duarte@bis2bis.com.br>
2025-05-20 11:17:27 -0300

    ✨ Styling of match cards

--- src/components/DashboardContent.vue
+++ src/components/DashboardContent.vue
@@ -1,71 +1,95 @@
 <template>
   <div class="bg-dashboard bg-no-repeat bg-cover bg-center p-5 pt-20 md:ml-64 w-65 h-screen overflow-y-auto">
     <main>
-      <div>
-        <h2 class="mb-4 text-2xl font-semibold text-gray-900 dark:text-white">Partidas</h2>
-
+      <div class="mb-4">
+        <h2 class="text-2xl font-semibold text-gray-900 dark:text-white">Partidas</h2>
+        <p class="text-sm text-gray-600">Rodada 9</p>
       </div>
-      <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mb-4">
-        <div class="border-2 border-dashed border-gray-300 rounded-lg dark:border-gray-600 h-32 md:h-64">
-          <h4>16 de maio de 20025</h4>
-          <p>Rodada 9</p>
-          <div class="scoreboard">
-            <div class="team">
-              <div class="team_shield">
-                <img src="" alt="" />
-                <span>Team Name</span>
+      <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mb-4">
+
+        <div class="bg-white/50 rounded-lg h-auto py-6 px-5 text-center">
+          <h4 class="font-semibold uppercase text-gray-900">16 maio 2025</h4>
+          <span class="text-sm text-gray-600">Partida 1</span>
+          <div class="scoreboard mt-3 flex justify-between items-center">
+            <div class="team flex justify-center items-center">
+              <div class="team_shield justify-between">
+                <img src="../assets/images/camisa-azul.png" alt="Time A" width="60" />
+                <span class="font-semibold text-gray-800">Time A</span>
               </div>
-              <div class="score">1</div>
+              <div class="score ms-5 text-3xl text-gray-800 font-semibold">1</div>
             </div>
 
-            <div class="versus">
-              <span>x</span>
-              <span>Em andamento</span>
+            <div class="versus flex flex-col">
+              <p class="text-3xl text-gray-800 mt-7">x</p>
+              <p class="text-xs font-semibold text-gray-800 mt-5">Encerrado</p>
             </div>
             
-            <div class="team">
-              <div class="team_shield">
-                <img src="" alt="" />
-                <span>Team Name</span>
+            <div class="team flex justify-center items-center flex-row-reverse">
+              <div class="team_shield justify-between">
+                <img src="../assets/images/camisa-branca.png" alt="Time B" width="60" />
+                <span class="font-semibold text-gray-800">Time B</span>
               </div>
-              <div class="score">2</div>
+              <div class="score me-5 text-3xl text-gray-800 font-semibold">2</div>
             </div>
           </div>
         </div>
-        <div class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-32 md:h-64">
-          <h4>16 de maio de 20025</h4>
-          <p>Rodada 9</p>
 
-        </div>
-        <div class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-32 md:h-64">
-          <h4>16 de maio de 20025</h4>
-          <p>Rodada 9</p>
+        <div class="bg-white/50 rounded-lg h-auto py-6 px-5 text-center">
+          <h4 class="font-semibold uppercase text-gray-900">16 maio 2025</h4>
+          <span class="text-sm text-gray-600">Partida 2</span>
+          <div class="scoreboard mt-3 flex justify-between items-center">
+            <div class="team flex justify-center items-center">
+              <div class="team_shield justify-between">
+                <img src="../assets/images/camisa-azul.png" alt="Time A" width="60" />
+                <span class="font-semibold text-gray-800">Time C</span>
+              </div>
+              <div class="score ms-5 text-3xl text-gray-800 font-semibold">2</div>
+            </div>
 
+            <div class="versus flex flex-col">
+              <p class="text-3xl text-gray-800 mt-7">x</p>
+              <p class="text-xs font-semibold text-gray-800 mt-5">Em andamento</p>
+            </div>
+            
+            <div class="team flex justify-center items-center flex-row-reverse">
+              <div class="team_shield justify-between">
+                <img src="../assets/images/camisa-branca.png" alt="Time B" width="60" />
+                <span class="font-semibold text-gray-800">Time D</span>
+              </div>
+              <div class="score me-5 text-3xl text-gray-800 font-semibold">2</div>
+            </div>
+          </div>
         </div>
-        <div class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-32 md:h-64">
-          <h4>16 de maio de 20025</h4>
-          <p>Rodada 9</p>
 
-        </div>
+        <div class="bg-white/50 rounded-lg h-auto py-6 px-5 text-center">
+          <h4 class="font-semibold uppercase text-gray-900">16 maio 2025</h4>
+          <span class="text-sm text-gray-600">Partida 3</span>
+          <div class="scoreboard mt-3 flex justify-between items-center">
+            <div class="team flex justify-center items-center">
+              <div class="team_shield justify-between">
+                <img src="../assets/images/camisa-azul.png" alt="Time A" width="60" />
+                <span class="font-semibold text-gray-800">Time E</span>
+              </div>
+              <div class="score ms-5 text-3xl text-gray-800 font-semibold">0</div>
+            </div>
+
+            <div class="versus flex flex-col">
+              <p class="text-3xl text-gray-800 mt-7">x</p>
+              <p class="text-xs font-semibold text-gray-800 mt-5">Pendente</p>
+            </div>
+            
+            <div class="team flex justify-center items-center flex-row-reverse">
+              <div class="team_shield justify-between">
+                <img src="../assets/images/camisa-branca.png" alt="Time B" width="60" />
+                <span class="font-semibold text-gray-800">Time F</span>
+              </div>
+              <div class="score me-5 text-3xl text-gray-800 font-semibold">0</div>
+            </div>
+          </div>
+        </div>        
+
       </div>
 
-      <div class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-96 mb-4"></div>
-
-      <div class="grid grid-cols-2 gap-4 mb-4">
-        <div class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72"></div>
-        <div class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72"></div>
-        <div class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72"></div>
-        <div class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72"></div>
-      </div>
-
-      <div class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-96 mb-4"></div>
-
-      <div class="grid grid-cols-2 gap-4">
-        <div class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72"></div>
-        <div class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72"></div>
-        <div class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72"></div>
-        <div class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72"></div>
-      </div>
     </main>
   </div>
 </template>
