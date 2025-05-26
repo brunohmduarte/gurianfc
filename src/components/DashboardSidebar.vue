c626aac3a2fc51149ed0df18ecb1bcabbcd96667
Bruno Duarte <bruno.meddua@gmail.com>
2025-05-26 07:36:39 -0300

    ➕ Backup 24/05

--- src/components/DashboardSidebar.vue
+++ src/components/DashboardSidebar.vue
@@ -6,7 +6,7 @@
   >
     <div class="overflow-y-auto py-5 px-3 h-full bg-white dark:bg-gray-800/75">
       <form action="#" method="GET" class="md:hidden mb-2">
-        <label for="sidebar-search" class="sr-only">Search</label>
+        <label for="sidebar-search" class="sr-only">Pesquisar</label>
         <div class="relative">
           <div
             class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none"
@@ -37,7 +37,7 @@
       <ul class="space-y-2">
         <li>
           <a
-            href="#"
+            href="/admin/dashboard"
             class="flex items-center p-2 text-base font-medium text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
           >
             <svg
@@ -50,7 +50,7 @@
               <path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path>
               <path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path>
             </svg>
-            <span class="ml-3">Overview</span>
+            <span class="ml-3">Dashboard</span>
           </a>
         </li>
 
@@ -75,7 +75,7 @@
               ></path>
             </svg>
             <span class="flex-1 ml-3 text-left whitespace-nowrap"
-              >Pages</span
+              >Gerenciamento</span
             >
             <svg
               aria-hidden="true"
@@ -94,7 +94,7 @@
           <ul id="dropdown-pages" class="hidden py-2 space-y-2">
             <li>
               <a
-                href="#"
+                href="ð/"
                 class="flex items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
                 >Settings</a
               >
